fb2bddbe78f5e1187dce6cc28fd884709009e4f0
Ernesto Montada Romero <e.montada0@gmail.com>
2019-04-14 11:45:25 +0200

    Filtered cleanup command

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ commander.addCommand(new Command("image", "img"), async function ([board = "a", 
 commander.addCommand(new Command("clean", "cln"), async function ([num = 1]) {    
     const msgs = await this.channel.fetchMessages({ limit: num })
 
-    let ms = msgs//.filter(m => m.author.id === bot.user.id)
+    let ms = msgs.filter(m => m.author.id === bot.user.id)
     
     if (ms.size === 1) return await ms.first().delete()
     
@@ -51,7 +51,7 @@ let bot = new Client({
 bot.on("message", async message => {
     if (message.author.id == bot.user.id || !(message.channel.id == "538747728763682817")) return
     await commander.process(message)
-    //await message.delete()
+    await message.delete()
 })
 
 bot.on("ready", () => {
