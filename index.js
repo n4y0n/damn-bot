c287e3a5d2987a9d820165de17f267fdfed40dab
Ernesto Montada Romero <e.montada0@gmail.com>
2019-04-14 21:56:00 +0200

    Created MyBot class for better encapsulation

--- index.js
+++ index.js
@@ -9,24 +9,48 @@ const start = Date.now()
 const Commander = require("./Commander")
 const Command = require("./Command")
 
-async function genMessage() {
-    if (message) return message
+class MyBot extends Client {
+    constructor(commands_prefix = "-", options = {}) {
+        super(options)
+        this.prefix = commands_prefix
 
-    const result = (await axios.get("https://icanhazdadjoke.com/", {
-        headers: {
-            Accept: "application/json"
-        }
-    })).data.joke
+        this.commander = new Commander(commands_prefix)
 
-    return result
+        this.on("message", async message => {
+            if (message.author.id == this.user.id || !(message.channel.id == "538747728763682817")) return
+            await commander.process(message)
+            await message.delete()
+        })
+    }
+    
+    async genMessage() {
+        if (message) return message
+
+        const result = (await axios.get("https://icanhazdadjoke.com/", {
+            headers: {
+                Accept: "application/json"
+            }
+        })).data.joke
+
+        return result
+    }
 }
 
-const commander = new Commander("-")
-commander.addCommand(new Command("image", "img"), async function ([board = "a", thread = null]) {
+
+let bot = new MyBot("-", {
+    disabledEvents: ["TYPING_START"],
+    messageCacheMaxSize: 25,
+    messageCacheLifetime: 120,
+    messageSweepInterval: 120
+})
+
+
+bot.addCommand(new Command("image", "img"), async function ([board = "a", thread = null]) {
     let m = await this.channel.send("WIP")
     setTimeout(() => m.delete(), 5000)
 })
-commander.addCommand(new Command("clean", "cln"), async function ([num = 1]) {    
+
+bot.addCommand(new Command("clean", "cln"), async function ([num = 1]) {    
     const msgs = await this.channel.fetchMessages({ limit: num })
 
     let ms = msgs.filter(m => m.author.id === bot.user.id)
@@ -37,22 +61,11 @@ commander.addCommand(new Command("clean", "cln"), async function ([num = 1]) {
     
     await this.channel.bulkDelete(ms, true)
 })
-commander.addCommand(new Command("say", "s"), async function (message = []) {
+
+bot.addCommand(new Command("say", "s"), async function (message = []) {
     await this.channel.send(message.join(" "))
 })
 
-let bot = new Client({
-    disabledEvents: ["TYPING_START"],
-    messageCacheMaxSize: 25,
-    messageCacheLifetime: 120,
-    messageSweepInterval: 120
-})
-
-bot.on("message", async message => {
-    if (message.author.id == bot.user.id || !(message.channel.id == "538747728763682817")) return
-    await commander.process(message)
-    await message.delete()
-})
 
 bot.on("ready", () => {
     console.log("Bot took: " + (new Date().getTime() - start) + "MS")
