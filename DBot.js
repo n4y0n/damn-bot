8f603d8c1c01d0356f3cec3cdef30c9ad5779607
Ernesto Montada Romero <e.montada0@gmail.com>
2019-04-18 10:32:55 +0200

    Add & remove component functionality

--- DBot.js
+++ DBot.js
@@ -1,5 +1,31 @@
+const crypto = require("crypto")
 const { Client } = require("discord.js")
 const Commander = require("./CommandProcessor")
+const Component = require("./Component")
+
+function circular(object) {
+    var i = 0;
+
+    return function (key, value) {
+        if (i !== 0 && typeof (object) === 'object' && typeof (value) == 'object' && object == value)
+            return '[Circular]';
+
+        if (i >= 29) // seems to be a harded maximum of 30 serialized objects?
+            return '[Unknown]';
+
+        ++i; // so we know we aren't using the original object anymore
+
+        return value;
+    }
+
+}
+
+function getkey(objtohash) {
+    const sha1 = crypto.createHash("sha1")
+    const stringthis = JSON.stringify(objtohash, circular(objtohash))
+    sha1.update(stringthis)
+    return sha1.digest("hex")
+}
 
 class DBot extends Client {
 
@@ -10,9 +36,9 @@ class DBot extends Client {
      */
     constructor(commands_prefix = "-", options) {
         super(options)
-        this.components = []
+        this.components = {}
         this.prefix = commands_prefix
-        
+
         this.commander = new Commander(commands_prefix, {
             hooks: {
                 async onFinishExecution(ok, command) {
@@ -33,8 +59,18 @@ class DBot extends Client {
     }
 
     addComponent(component) {
+        if (!(component instanceof Component)) return console.error(`${component} id not a componet`)
         component.install(this)
-        this.components.push(component)
+        this.components[getkey(component)] = component
+    }
+
+    removeComponent(component) {
+        if (!(component instanceof Component)) return console.error(`${component} is not a componet`)
+        const key = getkey(component)
+        const retrivedcomponent = this.components[key]
+        if (retrivedcomponent == null) return console.error(`${component} not installed in this bot`)
+        this.components[key] = null
+        retrivedcomponent.uninstall(this)
     }
 
     getChannel(id) {
