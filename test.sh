048d528d09d9fd865a32ae3a4f69ddcb700766cb
Ernesto Montada Romero <e.montada0@gmail.com>
2019-04-19 20:58:17 +0200

    Run the bot

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!bash
-winpty docker run --rm -e DELAY=1 -e TOKEN=$1 -v /app/node_modules -v //d/User/Desktop/Damn\ you\ delli:/app -it prons/damn-bot
+winpty docker run --rm -e TOKEN=$1 -it prons/damn-bot
