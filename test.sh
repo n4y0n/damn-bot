82f8f5a33a166864ed1dfe1847b6e502b3420668
Ernesto Montada Romero <e.montada0@gmail.com>
2019-05-05 10:28:13 +0200

    Bash so cool

--- test.sh
+++ test.sh
@@ -1,2 +1,15 @@
 #!bash
-winpty docker run --rm -e TOKEN=$1 -it prons/damn-bot node ./node_modules/nodemon/bin/nodemon.js .
+
+TOKEN="Unknown"
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	if [[ ${line:0:5} = TOKEN ]] ; then
+		TOKEN=$(echo $line | sed "s/TOKEN=//")
+	else
+		if [[ $TOKEN = "Unknown" ]] ; then
+			TOKEN=$1
+		fi
+	fi
+done < "./.env"
+
+winpty docker run --rm -e TOKEN=$TOKEN -it prons/damn-bot node ./node_modules/nodemon/bin/nodemon.js .
