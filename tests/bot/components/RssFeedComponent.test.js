6e28952f8814c8540cab49d296a2617ba3cbb53a
Ernesto Montada Romero <e.montada0@gmail.com>
2019-06-13 13:54:59 +0200

    Clarify tests

--- tests/bot/components/RssFeedComponent.test.js
+++ tests/bot/components/RssFeedComponent.test.js
@@ -3,19 +3,31 @@ const { MockRssAdapter } = require("../../utils/utils")
 const assert = require("assert")
 
 describe("RssFeedComponent", () => {
+
+    // Test 1
     it("should be named 'hello'", () => {
+        // Setup
         const rssadapter = MockRssAdapter()
         const rssFeedComponent = new RssFeedComponent(rssadapter, "hello")
 
-        assert(rssFeedComponent.getFeedName() === "hello")
+        // Act
+        const rss_feed_name = rssFeedComponent.getFeedName()
+
+        // Test
+        assert(rss_feed_name === "hello")
     })
 
+    // Test 2
     it("should have 538747728763682817 as first subscriber", () => {
+        // Setup
         const rssadapter = MockRssAdapter()
         const rssFeedComponent = new RssFeedComponent(rssadapter)
-
         rssFeedComponent.addChannel("538747728763682817")
 
-        assert(rssFeedComponent.subscribedChannels[0] === "538747728763682817")
+        // Act
+        const first_subscriber_channel = rssFeedComponent.subscribedChannels[0]
+
+        // Test
+        assert(first_subscriber_channel === "538747728763682817")
     })
 })
