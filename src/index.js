204f249bcdefc11bed4d82f30a424b8debfd8b31
Ernesto Montada Romero <e.montada0@gmail.com>
2019-08-08 23:01:26 +0200

    Monogatari watch order

--- src/index.js
+++ src/index.js
@@ -64,7 +64,24 @@ const mono = new Command('helpgatari', {
 
 mono.exec = async function (ctx) {
     const channel = ctx[Symbol.for('channel')]
-
+    let rm = new RichEmbed()
+    rm.setTitle('[ Hey don\'t panic! ]')
+    rm.addField("1)", "Bakemonogatari\n---")
+    rm.addField("2)", "Nisemonogatari\n---")
+    rm.addField("3)", "Nekomonogatari (kuro)\n---")
+    rm.addField("4) Monogatari Series S2", "Nekomonogatari (shiro)\n" +
+        "Kabukimonogatari\n" +
+        "Otorimonogatari\n" +
+        "Onimonogatari\n" +
+        "Koimonogatari\n" +
+        "---")
+    rm.addField("5)", "Hanamonogatari\n---")
+    rm.addField("6)", "Tsukimonogatari\n---")
+    rm.addField("7)", "Owarimonogatari\n---")
+    rm.addField("8)", "Koyomimonogatari\n---")
+    rm.addField("9)", "Owarimonogatari S2\n---")
+    rm.addField("10)", "Zoku Owarimonogatar\n---")
+    await channel.send(rm)
 }
 
 clear.exec = async function (ctx) {
@@ -100,7 +117,8 @@ help.exec = async function (ctx) {
 
 CPC.addCommand(clear)
 CPC.addCommand(help)
-CPC.addCommand(feeds)
+CPC.addCommand(mono)
+// CPC.addCommand(feeds)
 //#endregion
 
 //#region ***** Bot hooks *****
---
+++ src/utils/getCachedData.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+
+let cache = {}
+
+module.exports = function (...pathArgs) {
+  return new Promise((resolve, reject) => {
+    const keyPath = path.join(...pathArgs)
+    if (cache[keyPath]) return resolve(cache[keyPath])
+    fs.readFile(keyPath, function(err, data) {
+      if (err) return reject(err)
+      cache[keyPath] = data
+      resolve(data.toString())
+    })
+  })
+}
