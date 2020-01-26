2364c00feaf6bdf7655d1551a0a181cdf8bf0555
Ernesto Montada Romero <e.montada0@gmail.com>
2020-01-26 12:46:48 +0100

    ids

--- src/components/listeners/log-manager.js
+++ src/components/listeners/log-manager.js
@@ -1,16 +1,27 @@
 //@ts-check
 const Processor = require('../../interfaces/Processor')
 const logger = require('../../utils/logging')
+const {Message} = require('discord.js')
 
 class MessageLogManager extends Processor {
-    constructor () {
+    constructor (showID = false) {
         super()
+        this.showID = showID
     }
 
     async process (message) {
+        const guildname = message.guild.name
+        const guildid = message.guild.id
+
         const channelname = message.channel.name ? message.channel.name : "Private"
         const channelid = message.channel.id ? message.channel.id : "XXXXXXXXXXXXXXXXXX"
-        logger.verbose(`New Message from ${message.author.username}(${message.author.id}) in ${channelname}(${channelid}): ${message.content}`, { location: this })
+        const auid = message.author.id
+        
+        if (this.showID) {
+            logger.verbose(`${guildname}(${guildid})/${channelname}(${channelid}) -> ${message.author.username}(${auid}): ${message.content}`, { location: this })
+        } else {
+            logger.verbose(`${guildname}/${channelname} -> ${message.author.username}: ${message.content}`, { location: this })
+        }
         return false
     }
 
