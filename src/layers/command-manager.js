225c51ce07d8f5990a9de756f9d29ba9c113e53d
Ernesto Montada Romero <e.montada0@gmail.com>
2020-04-22 23:57:20 +0200

    to test

--- src/layers/command-manager.js
+++ src/layers/command-manager.js
@@ -7,29 +7,28 @@ module.exports = class CommandManager extends Layer {
     commands = new Map();
     prefix = "";
 
-    constructor () {
+    constructor() {
         super();
     }
 
-    async onMessage (message) {
+    async onMessage(message) {
         const { content } = message;
 
         if (content.substr(0, this.prefix.length) === this.prefix || !this.prefix) {
-            const [commandString, ...argumentList] = content.split(" ");            
+            const [commandString, ...argumentList] = content.split(" ");
             const command = commandString.replace(this.prefix, "");
 
             if (this.commands.has(command)) {
                 log.d("Recieved comand.");
                 log.d(content);
 
-                // TODO MIDDLEWARES
-                this.commands.get(command).exec(message, ...argumentList);
+                this.dispatch(this.commands.get(command).exec, message, ...argumentList);
 
                 log.d("-".repeat(20));
                 return true;
             }
         }
-        
+
         return false;
     }
 
@@ -40,7 +39,7 @@ module.exports = class CommandManager extends Layer {
 
     entries() { return this.commands.entries(); }
 
-    toString () {
+    toString() {
         return `CommandManager(prefix="${this.prefix}")`
     }
 
@@ -49,5 +48,28 @@ module.exports = class CommandManager extends Layer {
         cm.prefix = prefix;
         cm.commands = new Map(commands.map(c => ([c.name, c])));
         return cm;
+    }
+
+    dispatch(done, message, ...args) {
+        var idx = 0;
+        var stack = this.middlewares;
+        if (stack.length === 0) {
+            return done(message, ...args);
+        }
+
+        return next();
+
+        function next(err) {
+            if (err) {
+                return done(message, ...args);
+            }
+
+            let layer = stack[idx++];
+            if (!layer) {
+                return done(message, ...args);
+            }
+
+            layer(next, message, ...args);
+        }
     }
 }
