70ad031a191eb54ef60de880e90e116ad099b5a5
Ernesto Montada Romero <e.montada0@gmail.com>
2020-04-25 18:26:45 +0200

    commands

--- src/utils/termial-cli.js
+++ src/utils/termial-cli.js
@@ -3,28 +3,43 @@ const readline = require("readline")
 const log = require("./logging").getLogger("CliManager")
 
 async function init (bot) {
-    const channelsCache = new Map();
-    
+
+    const commands = new Map();
+
+    commands.set("say", async function (bot, ...args) {
+        // const channel = await bot.channels.get('538747728763682817'); //'670943087807299607'//
+        // const message = await channel.send("test boi.");
+        // await message.react("🙂");
+        // setTimeout(async () => await message.delete(), 3000);
+    });
+
+    commands.set("hw", async function (bot, ...args) {
+        log.d(args.join(" ") + " world");
+    });
+
     const interface = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
-    
+
     interface.on("line", async line => {
         const execute = parseCommand(line);
         await execute(bot);
     });
 
     function parseCommand(string) {
-        const args = parseWords(string);
+        const [command, ...args] = parseWords(string);
+        const execute = buildCommand(command, args)
+        return execute;
+    }
 
-        log.i(args)
-
+    function buildCommand(command, args = []) {
         return async bot => {
-            // const channel = await bot.channels.get('538747728763682817'); //'670943087807299607'//
-            // const message = await channel.send("test boi.");
-            // await message.react("🙂");
-            // setTimeout(async () => await message.delete(), 3000);
+            if (commands.has(command)) {
+                return commands.get(command)(bot, ...args);
+            } else {
+                log.i("Command \"" + command + "\" not found");
+            }
         }
     }
 
@@ -35,13 +50,15 @@ async function init (bot) {
 
         while(index < string.length) {
             let char = string[index++];
-            word += char;
 
             if (char === " " || index >= string.length) {
+                if (char !== " ") { word += char; }
                 strings.push(word);
                 word = "";
                 continue; 
             }
+
+            word += char;
 
             if (char === "\"") {
                 word = "";
