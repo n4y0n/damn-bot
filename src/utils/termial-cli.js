8874f130e99b6922f8e4a9caf89600a6e0facd26
Ernesto Montada Romero <e.montada0@gmail.com>
2020-04-26 09:52:03 +0200

    autocomplete

--- src/utils/termial-cli.js
+++ src/utils/termial-cli.js
@@ -6,7 +6,8 @@ async function init (bot) {
 
     const commands = new Map();
 
-    commands.set("say", async function (bot, ...args) {
+    commands.set("say", async function (bot, to, message) {
+        if (!to || !message) throw Error();
         // const channel = await bot.channels.get('538747728763682817'); //'670943087807299607'//
         // const message = await channel.send("test boi.");
         // await message.react("🙂");
@@ -14,23 +15,30 @@ async function init (bot) {
     });
 
     commands.set("hw", async function (bot, ...args) {
+        if (args.length <= 0) throw Error();
         log.d(args.join(" ") + " world");
+    });
+
+    commands.set("help", async function (bot) {
+        log.i("say <to> <message>");
+        log.i("hw <message>");
     });
 
     const interface = readline.createInterface({
         input: process.stdin,
-        output: process.stdout
+        output: process.stdout,
+        completer: completeCommand
     });
 
     interface.on("line", async line => {
+        if (!line) { return; }
         const execute = parseCommand(line);
-        await execute(bot);
+        try { await execute(bot); } catch(e) { await parseCommand("help")(bot); } finally { log.d("✨"); }
     });
 
     function parseCommand(string) {
         const [command, ...args] = parseWords(string);
-        const execute = buildCommand(command, args)
-        return execute;
+        return buildCommand(command, args);
     }
 
     function buildCommand(command, args = []) {
@@ -73,6 +81,25 @@ async function init (bot) {
         }
 
         return strings;
+    }
+
+    function strcmp (str1, str2, lgth) {
+        var s1 = (str1 + '')
+          .substr(0, lgth);
+        var s2 = (str2 + '')
+          .substr(0, lgth);
+      
+        return ((s1 == s2) ? 0 : ((s1 > s2) ? 1 : -1));
+    }
+
+    function completeCommand(line) {
+        let matchs = []
+        for (let [command, _] of commands.entries()) {
+            if (strcmp(command, line, line.length) != 0) { continue; }
+
+            matchs.push(command)
+        }
+        return [matchs, line]
     }
 }
 
