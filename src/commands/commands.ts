a88cffa43522f1614e1cde67b9c065ae6c60bec9
Ernesto Montada Romero <e.montada0@gmail.com>
2021-01-19 23:39:05 +0100

    added ytdl

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,7 +1,9 @@
-import { Message } from "discord.js";
+import { Client, Message } from "discord.js";
 import { get } from "../config";
 
 export async function commandHandler(message: Message) {
+    if (message.author.bot) return;
+
     if (message.isMentioned(get("client").user)) {
         console.log("[🛰️] mentioned by "+ message.author.username +".")
         await message.react("✨");
@@ -14,16 +16,18 @@ export async function commandHandler(message: Message) {
 }
 
 async function dispatchMessage(message: Message) {
+    const client: Client = get("client");
+
     if (message.isMentioned(get("client").user)) {
         message.content = message.content.replace("<@!" + get("client").user.id + ">", "").trim();
         if (!message.content) return;
     }
 
-    if (message.content === "entra") {
+    if (message.content === "play") {
         const voiceConnection = await message.member.voiceChannel.join();
     }
 
-    if (message.content === "esci") {
+    if (message.content === "stop") {
         message.member.voiceChannel.leave();
     }
 }
