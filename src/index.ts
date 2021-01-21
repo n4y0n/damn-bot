70bbffb7495e44f58ae744571205088d4f0d8696
Ernesto Montada Romero <e.montada0@gmail.com>
2021-01-21 11:05:21 +0100

    try riconnection on error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { commandHandler } from "./commands/commands";
-import { Client } from "discord.js";
+import { Channel, Client } from "discord.js";
 import { config as DotEnvInit } from "dotenv";
 import { config, get, set } from "./config";
 
@@ -9,11 +9,20 @@ config();
 const client = new Client();
 set("client", client);
 
-client.on("message", commandHandler)
+client.on("message", commandHandler);
 
 client.on("ready", () => {
     client.user.setActivity("prefix " + get("prefix"));
     console.log("[📡] Bot ready!");
-})
+});
+
+client.on("error", async e => {
+    console.log("[📡] Error, riconnecting...");
+    return client.login(process.env.TOKEN);
+});
+
+client.on("disconnect", (channel: Channel) => {
+    console.log("[📡] Disconnected from " + channel.id);
+});
 
 client.login(process.env.TOKEN);
