cc67459386beb1754a5f5bb6ef19603636169385
Ernesto Montada Romero <e.montada0@gmail.com>
2021-03-11 21:38:52 +0100

    debug logs

--- src/controller/universita.ts
+++ src/controller/universita.ts
@@ -2,24 +2,24 @@ import { RichEmbed } from "discord.js";
 
 export const fetchMaterialeReti: () => RichEmbed = () =>
 {
-
+	console.log("Unimplemented fetchMaterialeReti()");
 	return null;
 };
 
 export const fetchMaterialeSistemiOperativi: () => RichEmbed = () =>
 {
-
+	console.log("Unimplemented fetchMaterialeSistemiOperativi()");
 	return null;
 };
 
 export const fetchMaterialeAlgoritmi: () => RichEmbed = () =>
 {
-
+	console.log("Unimplemented fetchMaterialeAlgoritmi()");
 	return null;
 };
 
 export const fetchMaterialeBasiDati: () => RichEmbed = () =>
 {
-
+	console.log("Unimplemented fetchMaterialeBasiDati()");
 	return null;
 };
