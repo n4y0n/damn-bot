7c7e64c330841409020e6a9082af9ae6e951b97a
Ernesto Montada Romero <e.montada0@gmail.com>
2022-09-20 13:05:18 +0200

    idk

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,97 @@
-import { Channel, Client, Message, Permissions } from "discord.js";
+import {
+	Client,
+	IntentsBitField,
+	Interaction,
+	Message,
+	Routes,
+	SlashCommandBuilder,
+} from "discord.js";
+
 import debug from "debug";
 import { config } from "dotenv";
 import { get, load, setClient, Utils } from "./config";
-import { handleMessage } from "./controller/handler";
-import checkproc from "./controller/checkproc";
+import { REST } from '@discordjs/rest';
 
 const log = debug("bot:bot");
-
 // Load environments from .env
 config();
-
 // Load bot configurations from $HOME/.discord-bot.json
 load().then(main);
 
-function main() {
-	const client = new Client();
+
+async function main() {
+	const commands = []
+	const client = new Client({
+		intents: [
+			IntentsBitField.Flags.Guilds,
+			IntentsBitField.Flags.GuildMessages,
+			IntentsBitField.Flags.GuildMembers,
+			IntentsBitField.Flags.GuildEmojisAndStickers,
+			IntentsBitField.Flags.GuildScheduledEvents,
+		],
+	});
+	const rest = new REST({ version: '10' }).setToken(get("token"));
 	setClient(client);
 
-	client.on("message", (message: Message) => {
-		log("[📡] Message received");
-		if (!isForMe(message)) return;
-		sanitizeMessage(message);
 
-		Utils.logMessage(message);
-		handleMessage(message);
+	const clear = new SlashCommandBuilder()
+		.setName('clear')
+		.setDescription('Clears messages')
+		.setDMPermission(false);
+
+	const ping = new SlashCommandBuilder()
+		.setName('ping')
+		.setDMPermission(false)
+		.setDescription('Replies with Pong!');
+
+	commands.push(clear, ping);
+
+	client.on('interactionCreate', async (interaction: Interaction) => {
+		console.log(interaction);
+
+		if (!interaction.isChatInputCommand()) return;
+		if (interaction.commandName === 'ping') {
+			const int = await interaction.reply({
+				content: 'Pinging...',
+				ephemeral: true,
+			});
+			await interaction.editReply({
+				content: `Pong! Latency is ${(await int.awaitMessageComponent()).createdTimestamp - interaction.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`
+			});
+		}
 	});
 
 	client.on("ready", async () => {
 		const status = get("status");
 		const game = get("game");
 
-		await checkproc(client);
+		await rest.put(
+			Routes.applicationCommands(client.user.id),
+			{ body: commands },
+		);
 
-		await client.user.setActivity(game);
-		await client.user.setStatus(status);
-		const inviteUrl = await client.generateInvite(Permissions.ALL);
-		console.log("Use this url to make me join your server: %s", inviteUrl);
+		client.user.setActivity(game);
+		client.user.setStatus(status);
+		const inviteUrl = client.generateInvite();
 		log("[📡] Bot ready!");
-	});
-
-	client.on("error", (e) => {
-		log("[ERROR] %o", e);
-	});
-
-	client.on("disconnect", (channel: Channel) => {
-		log("[📡] Disconnected from " + channel.id);
+		console.log("Use this url to make me join your server: %s", inviteUrl);
 	});
 
 	client.login(get("token"));
 }
 
-function sanitizeMessage(message: Message) {
-	if(message.content.startsWith(get("prefix"))) {
-		message.content = message.content.substr(get("prefix").length);
+async function clear(message: Message, args: string[]) {
+	const num = parseInt(args[0]);
+	if (isNaN(num)) {
+		await message.channel.send("Please provide a number of messages to delete");
 		return;
 	}
-	if (message.isMentioned(message.client.user)) {
-		message.content = Utils.removeMention(message.content, message.client.user.id);
-	}
-}
 
-function isForMe(message: Message) {
-	if (message.author.bot) return false;
-
-	if (message.content.startsWith(get("prefix"))) {
-		return true;
+	if (num < 1 || num > 100) {
+		await message.channel.send("Please provide a number between 1 and 100");
+		return;
 	}
 
-	if (get("prefix") === "@mention" || get("altprefix") === "@mention") {
-		return message.isMentioned(message.client.user)
-	}
-	return false;
+	// const messages = await fetchMessages({ message, arguments: { num } });
+	// await deleteMessages({ message, arguments: { num } }, messages);
 }
